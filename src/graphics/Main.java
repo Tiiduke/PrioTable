51e097a88c1c874b83727a206a7fa65f436f7c39
Tiiduke <tiithendrik.piibeleht@gmail.com>
2016-05-10 12:52:29 +0300

    main gridpane resizing now works

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -1,18 +1,40 @@
 package graphics;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import static java.lang.Double.MAX_VALUE;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+    public void start(final Stage primaryStage) throws Exception{
+        Group root = new Group();
+        Scene scene = new Scene(root, 1300, 800);
+        final Pane pane = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+
+        GridPane mainGridPane = (GridPane) pane.getChildren().get(0);
+
+        mainGridPane.widthProperty().addListener(observable -> mainGridPane.resize(scene.getWidth(), scene.getHeight()));
+        mainGridPane.heightProperty().addListener(observable -> mainGridPane.resize(scene.getWidth(), scene.getHeight()));
+        mainGridPane.setGridLinesVisible(true);
+
+
+        pane.minHeightProperty().bind(scene.heightProperty());
+        pane.minWidthProperty().bind(scene.widthProperty());
+
+        root.getChildren().addAll(pane);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
