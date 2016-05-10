ce1204dbe3fa4447fc5c2c4e88f8b5c9fc8cc84e
Tiiduke <tiithendrik.piibeleht@gmail.com>
2016-05-10 13:42:25 +0300

    added method to access child gridpanes

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -14,6 +15,17 @@ import javafx.stage.Stage;
 import static java.lang.Double.MAX_VALUE;
 
 public class Main extends Application {
+
+    private GridPane getGridPaneFromMainGridPane(boolean urgent, boolean important, GridPane mainGridPane) {
+        if (urgent && important)
+            return (GridPane) mainGridPane.getChildren().get(4);
+        else if (urgent)
+            return (GridPane) mainGridPane.getChildren().get(5);
+        else if (important)
+            return (GridPane) mainGridPane.getChildren().get(6);
+        else
+            return (GridPane) mainGridPane.getChildren().get(7);
+    }
 
     @Override
     public void start(final Stage primaryStage) throws Exception{
@@ -27,6 +39,20 @@ public class Main extends Application {
         mainGridPane.heightProperty().addListener(observable -> mainGridPane.resize(scene.getWidth(), scene.getHeight()));
         mainGridPane.setGridLinesVisible(true);
 
+
+        for (Node node : mainGridPane.getChildren()) {
+            System.out.println(node);
+            if (node instanceof GridPane) {
+                ((GridPane) node).setGridLinesVisible(true);
+
+            }
+        }
+
+        GridPane urgImp = getGridPaneFromMainGridPane(true, true, mainGridPane);
+        urgImp.add(new Note("Tekst"), 0, 0);
+
+        GridPane notUrgImp = getGridPaneFromMainGridPane(false, true, mainGridPane);
+        notUrgImp.add(new Note("Tekst2"), 0, 0);
 
         pane.minHeightProperty().bind(scene.heightProperty());
         pane.minWidthProperty().bind(scene.widthProperty());
